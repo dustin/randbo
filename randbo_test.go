7f1b564ca7242d22bcc6e2128beb90d9fa38b9f0
Dustin Sallings <dustin@spy.net>
2014-04-28 16:14:29 -0700

    Added benchmark for crypto/rand to compare

--- randbo_test.go
+++ randbo_test.go
@@ -1,6 +1,7 @@
 package randbo
 
 import (
+	"crypto/rand"
 	"io"
 	"io/ioutil"
 	"testing"
@@ -25,5 +26,12 @@ func BenchmarkRandbo(b *testing.B) {
 	r := New()
 	for i := 0; i < b.N; i++ {
 		io.CopyN(ioutil.Discard, r, toCopy)
+	}
+}
+
+func BenchmarkCrypto(b *testing.B) {
+	b.SetBytes(toCopy)
+	for i := 0; i < b.N; i++ {
+		io.CopyN(ioutil.Discard, rand.Reader, toCopy)
 	}
 }
